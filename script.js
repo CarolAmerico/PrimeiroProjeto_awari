80714a92e2bdee353721fb971a3c2a2dfaba047a
CarolAmerico <carolameriico@gmail.com>
2022-08-13 11:11:23 -0300

    My first HTML page

---
+++ script.js
@@ -0,0 +1,19 @@
+noStyles();
+
+function atualizarNome() {
+  var nome = prompt('Insira um novo nome');
+  
+  var aluno = document.querySelector('h1');
+  aluno.textContent = 'Parabens ' + nome;
+}
+
+function noStyles() {
+  document.styleSheets[0].disabled = true;
+  document.styleSheets[1].disabled = true;
+}
+
+function reStyle(n) {
+  noStyles()
+  document.styleSheets[n].disabled = false;
+  atualizarNome();
+}
